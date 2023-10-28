c6dff463ddf4e19093ba7c4cea29ec2a6c9e89e7
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-10-28 23:18:10 +0100

    create home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,29 @@
 import Link from 'next/link';
 
-const Header = () => {
+import { UserButton, auth, currentUser } from '@clerk/nextjs';
+const Header = async () => {
+  const user = await currentUser();
+  const { userId }: { userId: string | null } = auth();
+
   return (
     <div
       className="w-full bg-blue-500 py-4 px-6 flex
       items-center justify-between mb-5"
     >
       <div>Bestwestern</div>
-      <div className="flex space-x-3">
-        <Link href="sign-in">sign in</Link>
-        <Link href="sign-up">sign up</Link>
+      <div className="flex items-center space-x-3 text-yellow-50">
+        <UserButton afterSignOutUrl="/" />
+        <div>{user?.firstName}</div>
       </div>
+
+      {!userId && (
+        <>
+          <div className="flex space-x-3">
+            <Link href="sign-in">sign in</Link>
+            <Link href="sign-up">sign up</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
-export default function Home() {
-  return <div>salam</div>;
+import Image from 'next/image';
+import Link from 'next/link';
+import { auth, currentUser } from '@clerk/nextjs';
+export default async function Home() {
+  const user = await currentUser();
+  const { userId }: { userId: string | null } = auth();
+  console.log(user);
+
+  return (
+    <div>
+      {userId && <div>salam</div>}
+      {!userId && (
+        <div className="flex flex-col space-y-3">
+          <div className="flex flex-col justify-center items-center p-4 space-y-3">
+            <h2 className=" text-lg font-bold">
+              Welcome to Western Hotel quality Food
+            </h2>
+            <p className="text-sm">
+              We value your feedback! Please take a moment to complete our
+              survey and help us improve your experience. Please{' '}
+              <Link className="text-blue-500" href="/sign-up">
+                Sign-up
+              </Link>
+            </p>
+          </div>
+          <Image
+            src="/image/homepagepic.jpg"
+            width={800}
+            height={600}
+            alt="Homepagepic"
+          />
+        </div>
+      )}
+    </div>
+  );
 }
