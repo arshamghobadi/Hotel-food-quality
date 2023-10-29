5dc58afe4e1b7bc8082176936f61152033fd6050
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-10-29 16:02:59 +0000

    compelet next and validate button of card

--- components/CardSurvey.tsx
+++ components/CardSurvey.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import {
@@ -29,9 +30,11 @@ import { survaySchema } from '@/validation/auth';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Image from 'next/image';
+import { cn } from '@/lib/utils';
 type Input = z.infer<typeof survaySchema>;
 
 const CardSurvey = () => {
+  const [formStep, setFormStep] = useState(0);
   const form = useForm<Input>({
     resolver: zodResolver(survaySchema),
     defaultValues: {
@@ -62,70 +65,140 @@ const CardSurvey = () => {
         <CardContent>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter your name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="room"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Room number</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter your Room number" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="food"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Food</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter Food" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="quality"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>quality</FormLabel>
-                    <FormControl>
-                      <Select onValueChange={field.onChange}>
-                        <SelectTrigger id="framework">
-                          <SelectValue placeholder="Select" />
-                        </SelectTrigger>
-                        <SelectContent id="quality" position="popper">
-                          <SelectItem value="bad">Bad</SelectItem>
-                          <SelectItem value="okey">Okey</SelectItem>
-                          <SelectItem value="good">Good</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button className="w-full" type="submit">
-                Submit
-              </Button>
+              <div
+                className={cn('', {
+                  hidden: formStep !== 0,
+                })}
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div
+                className={cn('', {
+                  hidden: formStep !== 1,
+                })}
+              >
+                <FormField
+                  control={form.control}
+                  name="room"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Room number</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your Room number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div
+                className={cn('', {
+                  hidden: formStep !== 2,
+                })}
+              >
+                <FormField
+                  control={form.control}
+                  name="food"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Food</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Food" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div
+                className={cn('', {
+                  hidden: formStep !== 3,
+                })}
+              >
+                <FormField
+                  control={form.control}
+                  name="quality"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>quality</FormLabel>
+                      <FormControl>
+                        <Select onValueChange={field.onChange}>
+                          <SelectTrigger id="framework">
+                            <SelectValue placeholder="Select" />
+                          </SelectTrigger>
+                          <SelectContent id="quality" position="popper">
+                            <SelectItem value="bad">Bad</SelectItem>
+                            <SelectItem value="okey">Okey</SelectItem>
+                            <SelectItem value="good">Good</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <Button
+                  className={cn('', {
+                    hidden: formStep === 0,
+                  })}
+                  onClick={() => {
+                    setFormStep(formStep - 1);
+                  }}
+                  type="button"
+                >
+                  back
+                </Button>
+                <Button
+                  className={cn(`${formStep === 0 ? 'w-full' : ''}`, {
+                    hidden: formStep === 3,
+                  })}
+                  onClick={() => {
+                    form.trigger(['name', 'room', 'food', 'quality']);
+                    const nameState = form.getFieldState('name');
+                    const roomState = form.getFieldState('room');
+                    const foodState = form.getFieldState('food');
+                    const qualityState = form.getFieldState('quality');
+                    if (formStep === 0 && !nameState.invalid) {
+                      return;
+                    } else if (formStep === 1 && !roomState.invalid) {
+                      return;
+                    } else if (formStep === 2 && !foodState.invalid) {
+                      return;
+                    } else if (formStep === 3 && !qualityState.invalid) {
+                      return;
+                    }
+                    setFormStep(formStep + 1);
+                  }}
+                  type="button"
+                >
+                  next
+                </Button>
+                <Button
+                  className={cn('', {
+                    hidden: formStep !== 3,
+                  })}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </div>
             </form>
           </Form>
         </CardContent>
