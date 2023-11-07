b218429b6e9b82a3518f64b4171783519b26acf0
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-11-07 16:34:12 +0000

    add api/webhook to public route

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,12 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', 'api/webhook/user'],
-  ignoredRoutes: ['/((?!api|trpc))(_next.*|.+.[w]+$)', '/api/webhook/user'],
+  publicRoutes: ['/', 'api/webhook/user', 'api/webhook'],
+  ignoredRoutes: [
+    '/((?!api|trpc))(_next.*|.+.[w]+$)',
+    '/api/webhook/user',
+    'api/webhook',
+  ],
 });
 
 export const config = {
